b51dbd8ac8be9195a83f29485e3d36edfda8da1c
Madeleine Negård <madelne@stud.ntnu.no>
2023-11-08 15:36:58 +0100

    Endret navn fra TrainDispatch til TrainDeparture

--- src/main/java/edu/ntnu/stud/TrainDeparture.java
+++ src/main/java/edu/ntnu/stud/TrainDeparture.java
@@ -1,6 +1,6 @@
 package edu.ntnu.stud;
 
-public class TrainDispatch {
+public class TrainDeparture {
     private int departure_time;
     private String line;
     private int trainNumber;
@@ -8,12 +8,14 @@ public class TrainDispatch {
     private int track;
     private int delay;
 
-    public TrainDispatch(int departure_time, String line, int trainNumber, String destination, int track, int delay) {
+    public TrainDeparture(int departure_time, String line, int trainNumber, String destination, int track, int delay) {
         this.departure_time = departure_time;
         this.line = line;
         this.trainNumber = trainNumber;
         this.destination = destination;
         this.track = track;
         this.delay = delay;
-  }
+    }
+
+
 }
