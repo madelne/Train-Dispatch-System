d56eec5786793b7a9bca647d3a2966cb91b57a4c
Madeleine Negård <madelne@stud.ntnu.no>
2023-11-08 15:31:01 +0100

    Laget TrainDispatch og TestClass klassene

---
+++ src/main/java/edu/ntnu/stud/TrainDispatch.java
@@ -0,0 +1,19 @@
+package edu.ntnu.stud;
+
+public class TrainDispatch {
+    private int departure_time;
+    private String line;
+    private int trainNumber;
+    private String destination;
+    private int track;
+    private int delay;
+
+    public TrainDispatch(int departure_time, String line, int trainNumber, String destination, int track, int delay) {
+        this.departure_time = departure_time;
+        this.line = line;
+        this.trainNumber = trainNumber;
+        this.destination = destination;
+        this.track = track;
+        this.delay = delay;
+  }
+}
--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -5,22 +5,4 @@ package edu.ntnu.stud;
  */
 public class TrainDispatchApp {
   
-  private int departure_time;
-  private String line;
-  private int trainNumber;
-  private String destination;
-  private int track;
-  private int delay;
-
-  public TrainDispatchApp(int departure_time, String line, int trainNumber, String destination, int track, int delay) {
-    this.departure_time = departure_time;
-    this.line = line;
-    this.trainNumber = trainNumber;
-    this.destination = destination;
-    this.track = track;
-    this.delay = delay;
-  }
-  
-  
-
 }
