4e1a9e8fa7a5ffbd3d452506d96af81febeb8f3d
Madeleine Negård <madelne@stud.ntnu.no>
2023-11-08 15:24:06 +0100

    Laget objekter og konstruktør til TrainDispatchApp

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -4,5 +4,23 @@ package edu.ntnu.stud;
  * This is the main class for the train dispatch application.
  */
 public class TrainDispatchApp {
-  // TODO: Fill in the main method and any other methods you need.
+  
+  private int departure_time;
+  private String line;
+  private int trainNumber;
+  private String destination;
+  private int track;
+  private int delay;
+
+  public TrainDispatchApp(int departure_time, String line, int trainNumber, String destination, int track, int delay) {
+    this.departure_time = departure_time;
+    this.line = line;
+    this.trainNumber = trainNumber;
+    this.destination = destination;
+    this.track = track;
+    this.delay = delay;
+  }
+  
+  
+
 }
