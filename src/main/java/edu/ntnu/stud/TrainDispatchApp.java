f941996602e75118af8148abbeee6770e4b2f541
Madeleine Negård <madelne@stud.ntnu.no>
2023-11-28 13:41:30 +0100

    made changes to init

--- src/main/java/edu/ntnu/stud/TrainDispatchApp.java
+++ src/main/java/edu/ntnu/stud/TrainDispatchApp.java
@@ -2,44 +2,40 @@ package edu.ntnu.stud;
 
 import java.time.LocalTime;
 import java.util.HashMap;
+import java.util.Scanner;
 
 /**
- * This is the main class for the train dispatch application.
+ * This is the user interface for the train dispatch system.
  *
  * @author Madeleine Negård
  */
 public class TrainDispatchApp {
 
-  /**
-   * The main method.
-   */
-
+  TrainDepartureRegister register = new TrainDepartureRegister();
 
   void init() {
-    TrainDeparture train1 = new TrainDeparture(LocalTime.of(17, 16), "L1", 
-        1, "Spikkestad", 4, LocalTime.of(0, 3));
-    TrainDeparture train2 = new TrainDeparture(LocalTime.of(14, 14), "L13", 
-        2, "Dal", 3);
-    TrainDeparture train3 = new TrainDeparture(LocalTime.of(15, 0), "F2", 
-        3, "Oslo Lufthavn", LocalTime.of(1, 0));
-    TrainDeparture train4 = new TrainDeparture(LocalTime.of(9, 7), "H3",  
-        4, "Hamar", 3, LocalTime.of(0, 13));
-    HashMap<Integer, TrainDeparture> trains = new HashMap<>();
-    trains.put(train1.getTrainNumber(), train1);
-    trains.put(train2.getTrainNumber(), train2);
-    trains.put(train3.getTrainNumber(), train3);
-    trains.put(train4.getTrainNumber(), train4);
-    TrainDepartureRegister register = new TrainDepartureRegister(trains);
+    register.addTrainDeparture(new TrainDeparture(LocalTime.of(17, 16), "L1", 
+        1, "Spikkestad", 4, LocalTime.of(0, 3)));
+    register.addTrainDeparture(new TrainDeparture(LocalTime.of(14, 14), "L13", 
+        2, "Dal", 3));
+    register.addTrainDeparture(new TrainDeparture(LocalTime.of(15, 0), "F2", 
+        3, "Oslo Lufthavn", LocalTime.of(1, 0)));
+    register.addTrainDeparture(new TrainDeparture(LocalTime.of(9, 7), "H3",  
+        4, "Hamar", 3, LocalTime.of(0, 13)));
     register.printTimeTable();
   }
   
   void start() {
-      
-      
+    System.out.println("1: Add train departure\n2: Remove train departure\n"
+        + "3: Remove all previous departures\n4: Search by train number\n"
+        + "5: Search by destination\n6: Add new register\n7: Add delay\n"
+        + "8: Print updated timetable\n9: Exit");
+    
+    
   }
   
   /**
-   * Fyll inn.
+   * This is the main method.
    *
    * @param args
    */
