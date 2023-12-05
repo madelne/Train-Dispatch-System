104c9bcf27467a1ed3867936f7ebb89261bd5433
Madeleine Negård <madelne@stud.ntnu.no>
2023-12-05 17:30:24 +0100

    added fields to TrainDepartureTest

--- src/test/java/edu/ntnu/stud/TrainDepartureTest.java
+++ src/test/java/edu/ntnu/stud/TrainDepartureTest.java
@@ -14,30 +14,31 @@ import org.junit.jupiter.api.TestTemplate;
 
 public class TrainDepartureTest {
   
-  public LocalTime currentTime = LocalTime.now().withSecond(0).withNano(0);
+  private LocalTime currentTime = LocalTime.now().withSecond(0).withNano(0);
+  private TrainDeparture trainDeparture;
 
   @Test
   void shouldInitialiseValidTrainDeparture() {
-    TrainDeparture train = new TrainDeparture(currentTime.plusHours(1), "C30", 
+    trainDeparture = new TrainDeparture(currentTime.plusHours(1), "C30", 
         123, "Lillestrøm", 3, LocalTime.of(0, 10));
-    assertEquals(currentTime.plusHours(1), train.getDepartureTime());
-    assertEquals("C30", train.getLine());
-    assertEquals(123, train.getTrainNumber());
-    assertEquals("Lillestrøm", train.getDestination());
-    assertEquals(3, train.getTrack());
-    assertEquals(LocalTime.of(0, 10), train.getDelay());
+    assertEquals(currentTime.plusHours(1), trainDeparture.getDepartureTime());
+    assertEquals("C30", trainDeparture.getLine());
+    assertEquals(123, trainDeparture.getTrainNumber());
+    assertEquals("Lillestrøm", trainDeparture.getDestination());
+    assertEquals(3, trainDeparture.getTrack());
+    assertEquals(LocalTime.of(0, 10), trainDeparture.getDelay());
   }
 
   @Test
   void shouldInitialiseValidTrainDepartureExtreme() {
-    TrainDeparture train = new TrainDeparture(currentTime.plusHours(1), "0", 
+    trainDeparture = new TrainDeparture(currentTime.plusHours(1), "0", 
         1, "/+-#.", 100, LocalTime.of(23, 0));
-    assertEquals(currentTime.plusHours(1), train.getDepartureTime());
-    assertEquals("0", train.getLine());
-    assertEquals(1, train.getTrainNumber());
-    assertEquals("/+-#.", train.getDestination());
-    assertEquals(100, train.getTrack());
-    assertEquals(LocalTime.of(23, 0), train.getDelay());
+    assertEquals(currentTime.plusHours(1), trainDeparture.getDepartureTime());
+    assertEquals("0", trainDeparture.getLine());
+    assertEquals(1, trainDeparture.getTrainNumber());
+    assertEquals("/+-#.", trainDeparture.getDestination());
+    assertEquals(100, trainDeparture.getTrack());
+    assertEquals(LocalTime.of(23, 0), trainDeparture.getDelay());
   }
 
   @Test
@@ -64,23 +65,23 @@ public class TrainDepartureTest {
     
   @Test
   void shouldInitialiseValidTrainDepartureWithoutDelay() {
-    TrainDeparture train = new TrainDeparture(currentTime.plusHours(1), "M26", 44, "Asker", 2);
-    assertEquals(currentTime.plusHours(1), train.getDepartureTime());
-    assertEquals("M26", train.getLine());  
-    assertEquals(44, train.getTrainNumber());
-    assertEquals("Asker", train.getDestination());
-    assertEquals(2, train.getTrack());
+    trainDeparture = new TrainDeparture(currentTime.plusHours(1), "M26", 44, "Asker", 2);
+    assertEquals(currentTime.plusHours(1), trainDeparture.getDepartureTime());
+    assertEquals("M26", trainDeparture.getLine());  
+    assertEquals(44, trainDeparture.getTrainNumber());
+    assertEquals("Asker", trainDeparture.getDestination());
+    assertEquals(2, trainDeparture.getTrack());
   }
 
   @Test
   void shouldInitialiseValidTrainDepartureWithoutDelayExtreme() {
-    TrainDeparture train = new TrainDeparture(currentTime.plusHours(1), "+/#æøå", 100, 
+    trainDeparture = new TrainDeparture(currentTime.plusHours(1), "+/#æøå", 100, 
         "+/#æøå", 100);
-    assertEquals(currentTime.plusHours(1), train.getDepartureTime());
-    assertEquals("+/#æøå", train.getLine());
-    assertEquals(100, train.getTrainNumber());
-    assertEquals("+/#æøå", train.getDestination());
-    assertEquals(100, train.getTrack());
+    assertEquals(currentTime.plusHours(1), trainDeparture.getDepartureTime());
+    assertEquals("+/#æøå", trainDeparture.getLine());
+    assertEquals(100, trainDeparture.getTrainNumber());
+    assertEquals("+/#æøå", trainDeparture.getDestination());
+    assertEquals(100, trainDeparture.getTrack());
   }
 
   @Test
@@ -104,24 +105,24 @@ public class TrainDepartureTest {
 
   @Test
   void shouldInitialiseValidTrainDepartureWithoutTrack() {
-    TrainDeparture train = new TrainDeparture(currentTime.plusHours(1), "R12", 21, 
+    trainDeparture = new TrainDeparture(currentTime.plusHours(1), "R12", 21, 
         "Gjøvik", LocalTime.of(0, 10));
-    assertEquals(currentTime.plusHours(1), train.getDepartureTime());
-    assertEquals("R12", train.getLine());
-    assertEquals(21, train.getTrainNumber());
-    assertEquals("Gjøvik", train.getDestination());
-    assertEquals(LocalTime.of(0, 10), train.getDelay());
+    assertEquals(currentTime.plusHours(1), trainDeparture.getDepartureTime());
+    assertEquals("R12", trainDeparture.getLine());
+    assertEquals(21, trainDeparture.getTrainNumber());
+    assertEquals("Gjøvik", trainDeparture.getDestination());
+    assertEquals(LocalTime.of(0, 10), trainDeparture.getDelay());
   }
 
   @Test
   void shouldInitialiseValidTrainDepartureWithoutTrackExtreme() {
-    TrainDeparture train = new TrainDeparture(currentTime.plusHours(1), "+/#æøå", 
+    trainDeparture = new TrainDeparture(currentTime.plusHours(1), "+/#æøå", 
         100, "+/#æøå", LocalTime.of(23, 0));
-    assertEquals(currentTime.plusHours(1), train.getDepartureTime());
-    assertEquals("+/#æøå", train.getLine());
-    assertEquals(100, train.getTrainNumber());
-    assertEquals("+/#æøå", train.getDestination());
-    assertEquals(LocalTime.of(23, 0), train.getDelay());
+    assertEquals(currentTime.plusHours(1), trainDeparture.getDepartureTime());
+    assertEquals("+/#æøå", trainDeparture.getLine());
+    assertEquals(100, trainDeparture.getTrainNumber());
+    assertEquals("+/#æøå", trainDeparture.getDestination());
+    assertEquals(LocalTime.of(23, 0), trainDeparture.getDelay());
   }
 
   @Test
@@ -146,21 +147,21 @@ public class TrainDepartureTest {
 
   @Test
   void shouldInitialiseValidTrainDepartureWithoutDelayAndTrack() {
-    TrainDeparture train = new TrainDeparture(currentTime.plusHours(1), "E9", 912, "Høvik");
-    assertEquals(currentTime.plusHours(1), train.getDepartureTime());
-    assertEquals("E9", train.getLine());
-    assertEquals(912, train.getTrainNumber());
-    assertEquals("Høvik", train.getDestination());
+    trainDeparture = new TrainDeparture(currentTime.plusHours(1), "E9", 912, "Høvik");
+    assertEquals(currentTime.plusHours(1), trainDeparture.getDepartureTime());
+    assertEquals("E9", trainDeparture.getLine());
+    assertEquals(912, trainDeparture.getTrainNumber());
+    assertEquals("Høvik", trainDeparture.getDestination());
   }
 
   @Test
   void shouldInitialiseValidTrainDepartureWithoutDelayAndTrackExtreme() {
-    TrainDeparture train = new TrainDeparture(currentTime.plusHours(1), 
+    trainDeparture = new TrainDeparture(currentTime.plusHours(1), 
         "+/#æøå", 100, "Blommenholm");
-    assertEquals(currentTime.plusHours(1), train.getDepartureTime());
-    assertEquals("+/#æøå", train.getLine());
-    assertEquals(100, train.getTrainNumber());
-    assertEquals("Blommenholm", train.getDestination());
+    assertEquals(currentTime.plusHours(1), trainDeparture.getDepartureTime());
+    assertEquals("+/#æøå", trainDeparture.getLine());
+    assertEquals(100, trainDeparture.getTrainNumber());
+    assertEquals("Blommenholm", trainDeparture.getDestination());
   }
 
   @Test 
@@ -181,28 +182,28 @@ public class TrainDepartureTest {
 
   @Test
   void shouldReturnValidDelay() {
-    TrainDeparture trainDeparture = new TrainDeparture(
+    trainDeparture = new TrainDeparture(
         currentTime.plusHours(3), "M7", 04, "Oslo", LocalTime.of(0, 10));
     assertEquals(LocalTime.of(0, 10), trainDeparture.getDelay());
   }
 
   @Test
   void shouldReturn0DelayWithNullDelay() {
-    TrainDeparture trainDeparture = new TrainDeparture(
+    trainDeparture = new TrainDeparture(
         currentTime.plusHours(3), "H3", 13, "Stabæk");
     assertEquals(LocalTime.of(0, 0), trainDeparture.getDelay());
   }
 
   @Test
   void shouldReturnDepartureTimeWithDelayWithValidDepartureAndDelay() {
-    TrainDeparture trainDeparture = new TrainDeparture(
+    trainDeparture = new TrainDeparture(
         currentTime.plusHours(5), "E09", 12, "Høvik", LocalTime.of(0, 5));
     assertEquals(currentTime.plusHours(5).plusMinutes(5), trainDeparture.departureTimeWithDelay());
   }
 
   @Test
   void shouldReturnDepartureTimeWithDelayPastMidnight() {
-    TrainDeparture trainDeparture = new TrainDeparture(
+    trainDeparture = new TrainDeparture(
         currentTime.plusHours(6), "K1", 22, "Asker", LocalTime.of(13, 0));
     assertEquals(currentTime.plusHours(6).plusHours(13), trainDeparture.departureTimeWithDelay());
   }
