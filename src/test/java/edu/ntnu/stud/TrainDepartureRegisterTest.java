79abafa98a172be670b3b6cbfdb67ffc83140e8f
Madeleine Negård <madelne@stud.ntnu.no>
2023-12-07 12:57:12 +0100

    added @BeforeEach to TrainDepartureRegister

--- src/test/java/edu/ntnu/stud/TrainDepartureRegisterTest.java
+++ src/test/java/edu/ntnu/stud/TrainDepartureRegisterTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalTime;
 import java.util.HashMap;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -20,6 +22,12 @@ public class TrainDepartureRegisterTest {
   private TrainDeparture trainDeparture3;
   private TrainDepartureRegister register1;
   private TrainDepartureRegister register2;
+
+  @BeforeEach
+  public void init() {
+    register1 = new TrainDepartureRegister();
+    register2 = new TrainDepartureRegister();
+  }
   
   @Test
   void shouldInitialiseTrainDepartureRegisterWithValidTrainDepartures() {
@@ -49,7 +57,6 @@ public class TrainDepartureRegisterTest {
     trainDeparture1 = new TrainDeparture(currentTime.plusMinutes(30), "C3", 30, 
         "Sandefjord", 3, LocalTime.of(0, 3));
     trainDepartures.put(trainDeparture1.getTrainNumber(), trainDeparture1);
-    register1 = new TrainDepartureRegister();
     register1.addTrainDeparture(trainDeparture1);
     assertEquals(trainDepartures, register1.getTrainDepartures());
   }
@@ -60,17 +67,14 @@ public class TrainDepartureRegisterTest {
         currentTime.plusMinutes(5), "B1", 123, "Oslo");
     trainDeparture2 = new TrainDeparture(
         currentTime.plusMinutes(4), "C2", 123, "Bergen");
-    register1 = new TrainDepartureRegister();
     register1.addTrainDeparture(trainDeparture1);
     register1.addTrainDeparture(trainDeparture2);
-    register2 = new TrainDepartureRegister();
     register2.addTrainDeparture(trainDeparture1);
     assertEquals(register2.getTrainDepartures(), register1.getTrainDepartures());
   }
 
   @Test
   void shouldNotAddDepartedTrainDepartures() {
-    register1 = new TrainDepartureRegister();
     register1.setCurrentTime(LocalTime.of(10, 0));
     trainDeparture1 = new TrainDeparture(LocalTime.of(10, 0).minusMinutes(3), "OD10", 
         100, "Blommenholm", 1);
@@ -84,7 +88,6 @@ public class TrainDepartureRegisterTest {
         100, "Blommenholm", 1);
     trainDeparture2 = new TrainDeparture(currentTime.plusHours(3), "C3", 30, 
         "Sandefjord", 3, LocalTime.of(0, 3));
-    register1 = new TrainDepartureRegister();
     register1.addTrainDeparture(trainDeparture1);
     register1.addTrainDeparture(trainDeparture2);
     trainDeparture3 = register1.searchByTrainNumber(100);
@@ -93,14 +96,12 @@ public class TrainDepartureRegisterTest {
 
   @Test
   void shouldThrowExceptionWith0TrainNumber() {
-    register1 = new TrainDepartureRegister();
     /*0 train number should throw IllegalArgumentException*/
     assertThrows(IllegalArgumentException.class, () -> register1.searchByTrainNumber(0));
   }
 
   @Test
   void shouldReturnNullIfTrainNumberDoesNotExistInRegister() {
-    register1 = new TrainDepartureRegister();
     assertEquals(register1.searchByTrainNumber(88), null);
   }
 
@@ -112,7 +113,6 @@ public class TrainDepartureRegisterTest {
         "Gjøvik", LocalTime.of(0, 15));
     trainDeparture3 = new TrainDeparture(LocalTime.now().plusHours(2), "C3", 30, 
         "Blommenholm", 3, LocalTime.of(0, 3));
-    register1 = new TrainDepartureRegister();
     register1.addTrainDeparture(trainDeparture1);
     register1.addTrainDeparture(trainDeparture2);
     register1.addTrainDeparture(trainDeparture3);
@@ -124,7 +124,6 @@ public class TrainDepartureRegisterTest {
 
   @Test
   void shouldThrowExceptionWithEmptyDestinationString() {
-    register1 = new TrainDepartureRegister();
     /*Empty string destination should throw IllegalArgumentException*/
     assertThrows(IllegalArgumentException.class, () -> register1.searchByDestination(""));
     
@@ -134,7 +133,6 @@ public class TrainDepartureRegisterTest {
   void shouldRemoveAllDepartedTrainDepartures() {
     trainDeparture1 = new TrainDeparture(
         currentTime.plusMinutes(3), "T15", 15, "Lillehammer");   
-    register1 = new TrainDepartureRegister();
     register1.addTrainDeparture(trainDeparture1);
     register1.setCurrentTime(currentTime.plusMinutes(10));
     assertEquals(new TrainDepartureRegister().getTrainDepartures(), register1.getTrainDepartures());
@@ -144,7 +142,6 @@ public class TrainDepartureRegisterTest {
   void shouldRemoveDeparturesDelayedToTomorrow() {
     trainDeparture1 = new TrainDeparture(
         currentTime.plusHours(1), "AM", 99, "Oslo", LocalTime.of(23, 0));
-    register1 = new TrainDepartureRegister();
     register1.addTrainDeparture(trainDeparture1);
     assertEquals(new TrainDepartureRegister().getTrainDepartures(), register1.getTrainDepartures());
   }
@@ -161,7 +158,6 @@ public class TrainDepartureRegisterTest {
     trainsSorted.put(trainDeparture2.getTrainNumber(), trainDeparture2);
     trainsSorted.put(trainDeparture1.getTrainNumber(), trainDeparture1);
     trainsSorted.put(trainDeparture3.getTrainNumber(), trainDeparture3);
-    register1 = new TrainDepartureRegister();
     register1.addTrainDeparture(trainDeparture1);
     register1.addTrainDeparture(trainDeparture2);
     register1.addTrainDeparture(trainDeparture3);
@@ -172,8 +168,7 @@ public class TrainDepartureRegisterTest {
   @Test
   void shouldRemoveTrainDepartureWithGivenTrainNumber() {
     trainDeparture1 = new TrainDeparture(
-        LocalTime.now().plusMinutes(3), "T15", 15, "Lillehammer");   
-    register1 = new TrainDepartureRegister();
+        currentTime.plusMinutes(3), "T15", 15, "Lillehammer");   
     register1.addTrainDeparture(trainDeparture1);
     register1.removeTrainDeparture(15);
     assertEquals(new TrainDepartureRegister().getTrainDepartures(), register1.getTrainDepartures());
@@ -181,7 +176,6 @@ public class TrainDepartureRegisterTest {
 
   @Test
   void shouldNotRemoveNonExistinTrainDeparture() {
-    register1 = new TrainDepartureRegister();
     assertFalse(register1.getTrainDepartures().containsKey(10));
     register1.removeTrainDeparture(10);
   }
@@ -193,7 +187,6 @@ public class TrainDepartureRegisterTest {
 
   @Test
   void shouldSetNewCurrentTimeWithValidParameter() {
-    register1 = new TrainDepartureRegister();
     LocalTime firstCurrentTime = currentTime;
     register1.setCurrentTime(currentTime.plusHours(3));
     assertEquals(firstCurrentTime.plusHours(3), register1.getCurrentTime());
@@ -201,9 +194,9 @@ public class TrainDepartureRegisterTest {
 
   @Test
   void shouldNotChangeCurrentTimeWithEarlierNewTime() {
-    register1 = new TrainDepartureRegister();
-    register1.setCurrentTime(currentTime.minusHours(1));
-    assertEquals(currentTime, register1.getCurrentTime());
+    register1.setCurrentTime(LocalTime.of(10, 0));
+    register1.setCurrentTime(LocalTime.of(9, 0));
+    assertEquals(LocalTime.of(10, 0), register1.getCurrentTime());
   }
 
 }
